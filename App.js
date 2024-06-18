9a7203cd3aa24dbc49e9ba8772f4bc3df8d65e0f
Khemz-Developer <jalithapramod@gmail.com>
2024-06-18 14:01:06 +0530

    2

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+  res.status(200).json({ message: 'Hello World!2' });
 });
 
 app.listen(port,()=>{
